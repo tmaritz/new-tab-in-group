c86c2bf221f046c952a8a3cb949f776df26a61ad
Toby Maritz <33519371+tmaritz@users.noreply.github.com>
2022-07-21 18:54:08 +0100

    edit shortcuts, fixed mv2/mv3 mix up

--- background.js
+++ background.js
@@ -1,14 +1,8 @@
 chrome.action.onClicked.addListener((tab) => {
-    chrome.scripting.executeScript({
-        target: {tabId: tab.id},
-        function: openGroupTap(tab)
-    });
-});
-
-function openGroupTap(tab) {
-        var cur = tab[0];
-        chrome.tabs.create({index: cur.index+1}, function(newTab) {
-            chrome.tabs.group({tabIds: newTab.id, groupId: cur.groupId}); 
+    chrome.tabs.query({currentWindow: true, active: true}, function(allTabs) {
+        var currentTab = allTabs[0];
+        chrome.tabs.create({index: currentTab.index+1}, function(newTab) {
+            chrome.tabs.group({tabIds: newTab.id, groupId: currentTab.groupId}); 
         });
-
-}
+    }); 
+});
